540cbe8eacd0b605d5fc43cc0b27bdce1186a1c4
mustapha335 <reezy77jay@gmail.com>
2022-04-22 17:00:44 +0100

    images fixed

--- client/src/components/LaunchArticle/LaunchArticle.js
+++ client/src/components/LaunchArticle/LaunchArticle.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import './style.scss';
 
 const LaunchArticle = ({ name, link, imgUrl, description }) => (
-    <div className="d-flex justify-content-center">
+    <div className="d-flex justify-content-center container">
         <Link to={{ pathname: link }} className="article" target="_blank" rel="noopener">
             <div className="article__header">
                 <h5 className="m-0 p-2">{name}</h5>
